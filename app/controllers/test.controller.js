768b59545b52536a8f653fee6572e9f1a286f005
gogito <supergogito1@gmail.com>
2021-06-24 22:59:02 +0700

    Update test.controller.js

--- app/controllers/test.controller.js
+++ app/controllers/test.controller.js
@@ -5,31 +5,33 @@ exports.getAreainfo = async (req, res) => {
     var finalArray = [];
     let parkinglotArray = await ParkingLot.find().lean().exec();
     for (let n = 0; n < parkinglotArray.length; n++) {
-    finalArray =  finalArray.concat( plfunc.extract_area_from_parkinglot(parkinglotArray[n]));
+        finalArray = finalArray.concat(plfunc.extract_area_from_parkinglot(parkinglotArray[n]));
     }
     res.send(finalArray);
-        
+
 };
 
 
 exports.simulate_edge = async (req, res) => {
-    res.send({message: "Process started"});
+    res.send({ message: "Process started" });
     var finalArray = [];
     let parkinglotArray = await ParkingLot.find().lean().exec();
     for (let n = 0; n < parkinglotArray.length; n++) {
-    finalArray =  finalArray.concat( plfunc.extract_area_from_parkinglot(parkinglotArray[n]));
+        finalArray = finalArray.concat(plfunc.extract_area_from_parkinglot(parkinglotArray[n]));
     }
 
-    for (u = 0; u < 1000000; u++ ){
-        await this.sleep(2000);
-    let random = plfunc.getRandomIntInclusive(0, (finalArray.length-1));
-    plfunc.send_update(finalArray[random]);
-    }
+    for (g = 0; g < 10; g++) {
         
+        for (u = 0; u < 100000; u++) {
+            await this.sleep(1000);
+            let random = plfunc.getRandomIntInclusive(0, (finalArray.length - 1));
+            plfunc.send_update(finalArray[random]);
+        }
+    }
 };
 
 exports.sleep = (ms) => {
     return new Promise((resolve) => {
-      setTimeout(resolve, ms);
+        setTimeout(resolve, ms);
     });
-  }   
+}
