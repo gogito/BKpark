a1d3ca3d594a8a7fba6ecd46ca865d59e091cd18
gogito <supergogito1@gmail.com>
2021-07-24 00:12:56 +0700

    Update test.controller.js

--- app/controllers/test.controller.js
+++ app/controllers/test.controller.js
@@ -2,12 +2,14 @@ const ParkingLot = require('../models/parkinglot.model.js');
 const plfunc = require('../function/parkinglots.function.js');
 
 exports.getAreainfo = async (req, res) => {
+    console.log("Start: " + new Date)
     var finalArray = [];
     let parkinglotArray = await ParkingLot.find().lean().exec();
     for (let n = 0; n < parkinglotArray.length; n++) {
         finalArray = finalArray.concat(plfunc.extract_area_from_parkinglot(parkinglotArray[n]));
     }
     res.send(finalArray);
+    console.log("Done: " + new Date)
 
 };
 
